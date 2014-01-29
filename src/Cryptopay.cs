a707dd23e12d5e6e4484de0ce57ec43285bad726
Alexander Ivanov <al.s.ivanov@gmail.com>
2014-01-29 12:51:52 +0400

    Add src and build

---
+++ src/Cryptopay.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Collections.Specialized;
+using Newtonsoft.Json;
+
+namespace CryptopayApi
+{
+    public class Cryptopay
+    {
+        private string apiKey { get; set; }
+        private const string CRYPTOPAY_URL = "https://cryptopay.me/api/v1/invoices/";
+        public Cryptopay(string apiKey) 
+        {
+            this.apiKey = apiKey;
+        }
+
+        public Invoice RequestInvoice(
+            int price,
+            Currency currency,
+            string description,
+            string id,
+            Dictionary<string, string> callbackParams,
+            string successRedirectUrl,
+            string errorRedirectUrl,
+            string callbackUrl
+        ) 
+        {
+            Invoice invoice = null;
+            using (var wb = new WebClient())
+            {
+                var data = new NameValueCollection();
+                data["api_key"] = apiKey;
+                data["price"] = price.ToString();
+                data["currency"] = Enum.GetName(typeof(Currency), currency); ;
+                if (description != null)
+                    data["description"] = description;
+                if (id != null)
+                    data["id"] = id;
+                if (callbackParams != null) 
+                {
+                    foreach (string key in callbackParams.Keys)
+                    {
+                        string callbackParamName = "callback_params[" + key.Trim() + "]";
+                        data[callbackParamName] = callbackParams[key];
+                    }
+                }    
+                if (successRedirectUrl != null)
+                    data["success_redirect_url"] = successRedirectUrl;
+                if (errorRedirectUrl != null)
+                    data["error_redirect_url"] = errorRedirectUrl;
+                if (callbackUrl != null)
+                    data["callback_url"] = callbackUrl;
+                var response = wb.UploadValues(CRYPTOPAY_URL, "POST", data);
+                var responseString = Encoding.Default.GetString(response);
+                System.Diagnostics.Debug.WriteLine(responseString);
+                invoice = JsonConvert.DeserializeObject<Invoice>(responseString);
+            }
+            return invoice;
+        }
+    }
+}
---
+++ src/Invoice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Specialized;
+using Newtonsoft.Json;
+
+namespace CryptopayApi
+{
+    public class Invoice
+    {
+        public string Uuid { set; get; }
+        public string Description { set; get; }
+        public string Status { set; get; }
+        [JsonProperty("btc_price")]
+        public string BtcPrice { set; get; }
+        [JsonProperty("btc_address")]
+        public string BtcAddress { set; get; }
+        [JsonProperty("short_id")]
+        public string ShortId { set; get; }
+        [JsonProperty("callback_params")]
+        public Dictionary<string, string> CallbackParams { set; get; }
+        [JsonProperty("id")]
+        public string Id { set; get; }
+        public string Price { set; get; }
+        public string Currency { set; get; }
+        [JsonProperty("created_at")]
+        public string CreatedAt { set; get; }
+        [JsonProperty("valid_till")]
+        public string ValidTill { set; get; }
+    }
+}
